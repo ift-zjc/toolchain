5332898fe67fdf224f772b10db95127c01bc4a8b
Zhijiang Chen <zhijiang.chen@intfusiontech.com>
2018-04-16 11:57:28 -0400

    Project Init

---
+++ src/main/java/com/ift/toolchain/ToolchainApplication.java
@@ -0,0 +1,12 @@
+package com.ift.toolchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolchainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolchainApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ift/toolchain/ToolchainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ift.toolchain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToolchainApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
