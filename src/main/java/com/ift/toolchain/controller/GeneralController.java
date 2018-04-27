0f471cee72a009342f7879aa2cf2c722e4c65cfb
zhijiang chen <zhijiang@chen.me>
2018-04-27 17:30:06 -0400

    Rename orb file with satellite name .txt

--- src/main/java/com/ift/toolchain/controller/GeneralController.java
+++ src/main/java/com/ift/toolchain/controller/GeneralController.java
@@ -3,11 +3,18 @@ package com.ift.toolchain.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import static com.ift.toolchain.util.CommonUtil.ecef2lla;
+
 @Controller
 public class GeneralController {
 
     @GetMapping(value = "/")
     public String index(){
+
+        double[] data = ecef2lla(14788.5621*1000, -894.4494*1000, -21883.5687*1000);
+
+        System.out.println(data[0]+"/"+data[1]+"/"+data[2]);
+
         return "index";
     }
 }
