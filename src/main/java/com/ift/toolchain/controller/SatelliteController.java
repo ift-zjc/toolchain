6c9928e4878d064e8c7ecc88ade26e3052e63f64
lastcow <zhijiang@chen.me>
2018-05-02 00:20:02 -0400

    Merge branch 'master' of /Users/lastcow/Projects/toolchain with conflicts.

--- src/main/java/com/ift/toolchain/controller/SatelliteController.java
+++ src/main/java/com/ift/toolchain/controller/SatelliteController.java
@@ -4,7 +4,6 @@ package com.ift.toolchain.controller;
 import com.ift.toolchain.Service.SatelliteService;
 import com.ift.toolchain.dto.SatelliteCollection;
 import com.ift.toolchain.dto.SatelliteDto;
-import com.ift.toolchain.model.Orbit;
 import com.ift.toolchain.model.Satellite;
 import com.ift.toolchain.util.CommonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +14,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -24,27 +25,18 @@ public class SatelliteController {
     @Autowired
     SatelliteService satelliteService;
 
-
-
-
     @GetMapping(value = "/api/satellite/init")
-    public List<SatelliteCollection> initSatelliteData(){
+    public Map<String, List<SatelliteDto>> initSatelliteData(){
 
-        List<SatelliteCollection> satelliteCollections = new ArrayList<>();
-        String satellitePositionFolder = "C:\\Users\\zhijiang\\Documents\\Projects\\toolchain\\Crosslink Scenario Data\\Orbit Information";
+        SatelliteCollection satelliteCollection = new SatelliteCollection();
+        String satellitePositionFolder = "/Users/lastcow/Projects/toolchain/Crosslink Scenario Data/Orbit Information";
+        Map<String, List<SatelliteDto>> satelliteDtoMap = new HashMap<>();
         try {
             Files.list(Paths.get(satellitePositionFolder))
                     .filter(Files::isRegularFile)
                     .forEach(fileName -> {
 
                         String satelliteName = fileName.getFileName().toString().replaceAll(".txt", "");
-
-                        // Get orb name for this satellite
-                        Satellite satellite = satelliteService.findByName(satelliteName);
-                        List<Satellite> satellites = satelliteService.getSatelliesOnSameOrb(satelliteName);
-                        List<Satellite> satelliteSorted = satellites.stream().sorted(Comparator.comparing(Satellite::getOrderOnOrbit)).collect(Collectors.toList());
-
-
                         try (BufferedReader br = Files.newBufferedReader(fileName)) {
                             List<SatelliteDto> satelliteDtos = br.lines().skip(7).limit(1440)
                                     .filter(
@@ -52,50 +44,18 @@ public class SatelliteController {
                                     .map(line -> {
                                         String[] lineData = line.split(",");
                                         try {
+                                            // Get orb name for this satellite
+                                            //Satellite satellite = satelliteService.findByName(satelliteName);
                                             double[] cartesian3 = {Double.parseDouble(lineData[1])*1000, Double.parseDouble(lineData[2])*1000, Double.parseDouble(lineData[3])*1000};
-                                            SatelliteDto satelliteDto = new SatelliteDto();
-                                            satelliteDto.setTime((long)Double.parseDouble(lineData[0]));
-                                            satelliteDto.setCartesian3(cartesian3);
+                                            return new SatelliteDto((long)Double.parseDouble(lineData[0]), cartesian3) ;//, satellite.getOrbit().getName());
+                                        }catch (NumberFormatException ex){
 
-
-
-                                            return satelliteDto;
-                                        }catch (Exception ex){
-//                                            System.out.println(ex.getMessage());
                                         }
 
                                         return null;
 
                                     }).collect(Collectors.toList());
-
-                            SatelliteCollection satelliteCollection = new SatelliteCollection();
-
-                            // Find selected satellite index.
-                            int satelliteIndex = 0;
-                            for(int index = 0; index<satelliteSorted.size(); index++){
-                                if(satellite.getId().equalsIgnoreCase(satelliteSorted.get(index).getId())){
-                                    satelliteIndex = index;
-                                    break;
-                                }
-                            }
-
-                            if(satelliteIndex >0){
-                                if(satelliteIndex<satelliteSorted.size()-1){
-                                    satelliteCollection.setLeftSatelliteName(satelliteSorted.get(satelliteIndex-1).getName());
-                                    satelliteCollection.setRightSatelliteName(satelliteSorted.get(satelliteIndex+1).getName());
-                                }else{
-                                    satelliteCollection.setLeftSatelliteName(satelliteSorted.get(satelliteIndex-1).getName());
-                                    satelliteCollection.setRightSatelliteName(satelliteSorted.get(0).getName());
-                                }
-                            }else{
-                                satelliteCollection.setLeftSatelliteName(satelliteSorted.get(satelliteSorted.size()-1).getName());
-                                satelliteCollection.setRightSatelliteName(satelliteSorted.get(satelliteIndex+1).getName());
-                            }
-
-                            satelliteCollection.setName(satelliteName);
-                            satelliteCollection.setSatellites(satelliteDtos);
-
-                            satelliteCollections.add(satelliteCollection);
+                            satelliteDtoMap.put(satelliteName, satelliteDtos);
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
@@ -104,6 +64,6 @@ public class SatelliteController {
             e.printStackTrace();
         }
 
-        return satelliteCollections;
+        return satelliteDtoMap;
     }
 }
